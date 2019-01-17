386ff775edc0d8849572dc23d68cb10cefa40c9f
Frank Bauer <developer@ambertation.de>
2019-01-17 02:31:55 +0100

    Fixes for latest TeaVM

--- compiler/src/main/java/org/teavm/javac/Client.java
+++ compiler/src/main/java/org/teavm/javac/Client.java
@@ -66,8 +66,6 @@ import org.teavm.jso.impl.JSOPlugin;
 import org.teavm.jso.typedarrays.ArrayBuffer;
 import org.teavm.jso.typedarrays.Int8Array;
 import org.teavm.model.ClassHolderSource;
-import org.teavm.model.MethodReference;
-import org.teavm.model.ValueType;
 import org.teavm.parsing.CompositeClassHolderSource;
 import org.teavm.parsing.DirectoryClasspathClassHolderSource;
 import org.teavm.platform.plugin.PlatformPlugin;
@@ -290,9 +288,10 @@ public final class Client {
             new JCLPlugin().install(teavm);
             log("Plugins loaded in " + (System.currentTimeMillis() - pluginInstallationStart) + " ms");
 
-            teavm.entryPoint("main", new MethodReference(mainClass, "main", ValueType.parse(String[].class),
+            /*teavm.entryPoint("main", new MethodReference(mainClass, "main", ValueType.parse(String[].class),
                     ValueType.VOID))
-                    .withArrayValue(1, "java.lang.String");
+                    .withArrayValue(1, "java.lang.String");*/
+            teavm.entryPoint(mainClass, "main");
             File outDir = new File("/js-out");
             outDir.mkdirs();
 
